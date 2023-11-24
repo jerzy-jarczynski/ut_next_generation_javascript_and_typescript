ce7aced865f388f43477fd23c70431bc03e082f9
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-24 09:50:58 +0100

    Complete 50. Arrow Functions

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,24 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
-if (age > 20) {
-  let isOld = true;
-}
+// if (age > 20) {
+//   let isOld = true;
+// }
 
-console.log(isOld);
+// console.log(isOld);
+
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+printOutput(add(5, 2));
