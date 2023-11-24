c93fc79806809bf35055c81043f5903774c37c1e
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-24 09:54:45 +0100

    Complete 51. Default Function Parameters

--- src/app.ts
+++ src/app.ts
@@ -16,12 +16,14 @@ age = 29;
 
 // console.log(isOld);
 
-const add = (a: number, b: number) => a + b;
-
-console.log(add(2, 5));
+const add = (a: number, b: number = 1) => a + b;
 
 const printOutput: (a: number | string) => void = output => console.log(output);
 
 const button = document.querySelector('button');
 
-printOutput(add(5, 2));
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5));
